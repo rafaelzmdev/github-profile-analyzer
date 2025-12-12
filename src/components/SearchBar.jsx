4e3439ef1a9088bb12093662458b108a090b6d11
rafaelzmdev <rafaelzmdev@gmail.com>
2025-12-12 09:40:00 -0300

    missing a few quick fixes
    need to increase fetch's directory for sharing the api's response between components; need to implement dynamic textarea placeholder changing for better responsiveness, ease-of-use and general quality.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,5 @@
 import { data } from 'autoprefixer';
-import '../app.css'
+import '../App.css'
 import { useState, useEffect } from "react";
 
 
@@ -45,7 +45,7 @@ function Search() {
     return (
         <div className="flex pt-1 ml-2">
             <textarea className="min-w-420 font-normal overflow-x-hidden whitespace-nowrap pl-2 pr-2 p-1 border-4 rounded-3xl border-gray-700 dark:border-gray-200 bg-gray-300 dark:bg-gray-600 resize-none outline-none text-3xl max-h-14" wrap="off" placeholder="Enter username here" spellCheck="false" onChange={handleInputChange}></textarea>
-            <i className='bxr  bx-search font-bold pl-2 pt-1 text-5xl cursor-pointer hover:-translate-y-0.5 transition-transform' onClick={handleClick}></i>
+            <i className="bx bx-search font-bold pl-2 pt-1 text-5xl cursor-pointer hover:-translate-y-0.5 transition-transform" onClick={handleClick}></i>
         </div>
         
     );
