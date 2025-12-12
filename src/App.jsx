52f62aeec1e352f6b9639524802b332aedf77413
rafaelzmdev <rafaelzmdev@gmail.com>
2025-12-12 11:56:51 -0300

    fetchAPI properly situated
    Code is working absolutely smoothly now. And I know how to pass and inherit props between parent and children components. Which means, I can pass down the "info" prop, inherit in any of my other elements and start displaying the data fetched from the API.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,52 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import './components/Charts'
 import './components/RepoCard'
 import Search from './components/SearchBar'
 import Titlelogo from './components/Titlelogo'
 import './components/UserCard'
+import { data } from 'autoprefixer'
 
 function App() {
-  useEffect(() =>
-    document.documentElement.classList.add('dark')
-  );
+  const [info, setInfo] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    document.documentElement.classList.add('dark');
+  }, []);
+  
+  const fetchGithubApi = async (username) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) throw new Error('Failed to fetch API');
+      const data = await response.json();
+      setInfo(data);
+    }  catch (err) {
+        setError(err.message);
+    }  finally {
+        console.log("API Fetched sucessfuly")
+        console.log(data)
+        setLoading(false)
+    }
+  };
+
+
+  const handleClick = () => {
+    console.log("Search button clicked.")
+    if (username) {
+      fetchGithubApi(username)
+    } else {
+      setError("Please enter a username.")
+    }
+  };
+  const handleInputChange = e => {
+    setUsername(e.target.value);
+  };
+
   return (
     <>
       <div className="root min-h-screen bg-white dark:bg-black text-black dark:text-white  border-black dark:border-white">
@@ -17,11 +54,11 @@ function App() {
           <Titlelogo></Titlelogo>
         </div>
         <div className="search max-w-sameastitle">
-          <Search></Search>
+          <Search username={username} handleClick={handleClick} handleInputChange={handleInputChange}></Search>
         </div>
       </div>
     </>
   )
-}
+};
 
 export default App
