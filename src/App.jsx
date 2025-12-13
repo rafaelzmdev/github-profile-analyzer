9392a230b141ea067b34cf2db406d5a85be183a6
rafaelzmdev <rafaelzmdev@gmail.com>
2025-12-13 14:13:18 -0300

    last commit of the day
    added basic information about the user on the user chart. realized I will have to do an entire redesign of the backend to transfer the right data between the modules.

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ function App() {
   const fetchGithubApi = async (username) => {
     setLoading(true);
     setError(null);
+    
     try {
       const response = await fetch(`https://api.github.com/users/${username}`);
       if (!response.ok) throw new Error('Failed to fetch API');
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,16 +1,17 @@
 import '../App.css'
-import { useState, useEffect } from "react";
 function Card({info}) {
     if (!info) {
         return <div></div>
     }
-    
-
-
+    console.log(info)
     return(
         <div className="imgcontainer w-29.5 flex flex-col align-top justify-left">
             <img src={info.avatar_url} alt="User avatar" className="border-solid border-gray-300 border-opacity-45 backdrop-blur-[0.8px] dark:border-gray-700 dark:border-opacity-45 dark:backdrop-blur-[0.8px] border-8 rounded-128 w-72"></img>
-            <h1>sum testing</h1>
+            <div className="datacontainer w-72 flex flex-col items-center mt-[-0.5rem] gap-1.5">
+                <p className="username text-[3rem]">{info.login}</p>
+                <p className="follows text-[1.25rem]">{info.followers} Followers; Following {info.following}</p>
+                <p className="reposnumber text-[1.25rem]">Public repositories: {info.public_repos}</p>
+            </div>
         </div>
     )
 }
