540bad8da6a2231d0fc075e760ff7ca43675ea4c
rafaelzmdev <rafaelzmdev@gmail.com>
2026-02-07 13:48:17 -0300

    changed proxies
    seeing if the backend fetch will be sucessful already

--- publish_directory/vite.config.mjs
+++ publish_directory/vite.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://graphql-gha-api.onrender.com",
         changeOrigin: true,
         secure: false,
       },
