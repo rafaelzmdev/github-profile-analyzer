3ba131308482fd46eea04d4731c755b9a835f9a5
rafaelzmdev <rafaelzmdev@gmail.com>
2026-04-16 16:30:54 -0300

    quick polish
    so I don't look foolish

--- publish_directory/src/components/Charts.jsx
+++ publish_directory/src/components/Charts.jsx
@@ -6,16 +6,16 @@ function Charts({info, graphdata}) {
     return (
         <div className="grid grid-cols-2 grid-rows-2 gap-4">
             <div>
-                <h1 className="text-5xl">damn</h1>
+                <h1 className="text-5xl">Placeholder 1 (pie or small graph)</h1>
             </div>
             <div>
-                <h1 className="text-5xl">damn the second</h1>
+                <h1 className="text-5xl">Placeholder 2 (big graph)</h1>
             </div>
             <div>
-                <h1 className="text-5xl">damn the third</h1>
+                <h1 className="text-5xl">Placeholder 3 (another small graph)</h1>
             </div>
             <div>
-                <h1 className="text-5xl">damn the fourth</h1>
+                <h1 className="text-5xl">Fetch worked apparently ;D</h1>
             </div>
         </div>
     );
